889c2f156f83b37f1a5b905d7100afc8de5a0cd1
Athlan <athlan@vgroup.pl>
2013-12-21 06:02:06 +0100

    TOC creation in script fixed.

--- bin/printpdf.sh
+++ bin/printpdf.sh
@@ -2,3 +2,7 @@
 
 rm -rfv ./target/*
 pdflatex --output-directory=./target/ Main.tex
+pdflatex --output-directory=./target/ Main.tex
+
+# Run script twice, because .toc file is created
+# and considered as Table of Contents
